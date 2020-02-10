6771026a5ea1a7311726ca7d96fe5806d1099f45
Amy Barba <Amybarba70@gmail.com>
2020-02-09 22:36:57 -0800

    messages coding and function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,6 +9,8 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost: 5000';
 
   useEffect(() => {
@@ -31,7 +33,34 @@ const Chat = ({ location }) => {
 
   }, [ENDPOINT, location.search]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on('message', (message) => {
+setMessages([...messages, message])
+    })
+  }, [message])
+
+// function for sending messages
+const sendMessage = (event) => {
+event.preventDefault();
+
+  if(message) {
+    socket.emit('sendMessage', message, () => setMessage(''));
+  }
+}
+
+console.log(message, messages);
+
+
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input value={message} onChange={(event) => setMessage(event.target.value)}
+        onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} />
+
+      </div>
+    </div>
+  ) 
 }
 
 export default Chat;
