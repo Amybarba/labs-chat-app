c1411dc808c04770b48c9a450ec3a7f0e213e18c
Amy Barba <Amybarba70@gmail.com>
2020-02-10 01:39:25 -0800

    text message coding and debugging

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,65 +2,77 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
-// import './Chat.css';
+import Input from "../Input/Input.js";
+import InfoBar from "../InfoBar/InfoBar.js";
+import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
+
+import "./Chat.css";
 
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost: 5000';
+  const ENDPOINT = "localhost: 5000";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name);
 
-    socket.emit('join', { name, room }, () => {
-
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
-
-    return() => {
-      socket.emit('disconnect');
-
-      socket.off();
-    }
-
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
     socket.on('message', (message) => {
-setMessages([...messages, message])
+      setMessages([...messages, message ]);
+    });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
     })
-  }, [message])
+  
+    return () => {
+      socket.emit('disconnect');
 
-// function for sending messages
-const sendMessage = (event) => {
-event.preventDefault();
+      socket.off();
+    };
+  }, [messages])
 
-  if(message) {
-    socket.emit('sendMessage', message, () => setMessage(''));
+  // function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
   }
-}
-
-console.log(message, messages);
-
-
 
   return (
     <div className="outerContainer">
       <div className="container">
-        <input value={message} onChange={(event) => setMessage(event.target.value)}
-        onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} />
-
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
+      <TextContainer users={users} />
     </div>
-  ) 
-}
+  );
+};
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -22,6 +22,8 @@ socket.emit('message', { user: 'admin', text: `$(user.name), welcome to the room
 socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `$(user.name), has joined!` });
     socket.join(user.room);
 
+io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+
     callback();
   });
 
@@ -29,13 +31,17 @@ socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `$(user.na
 const user = getUser(socket.id);
 
 io.to(user.room).emit('message', { user: user.name, text: message });
-
+io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
 callback();
   })
 
   socket.on("disconnect", () => {
-    console.log("User has left!!!");
-  });
+  const user = removeUser(socket.id);
+
+  if(user){
+    io.to(user.room).getMaxListeners('message', { user: 'admin', text: `${user.name} has left.`})
+  }
+  })
 });
 
 app.use(router);
